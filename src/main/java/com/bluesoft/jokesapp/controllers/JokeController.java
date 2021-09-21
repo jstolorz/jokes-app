0e874f94882c21a6aebcd3fc3463db5ac6afa8f7
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-09-21 13:56:29 +0200

    adding services and controller for joke. Adding thymeleaf template

---
+++ src/main/java/com/bluesoft/jokesapp/controllers/JokeController.java
@@ -0,0 +1,23 @@
+package com.bluesoft.jokesapp.controllers;
+
+import com.bluesoft.jokesapp.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(final JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/",""})
+    public String showJoke(Model model){
+        model.addAttribute("joke",jokeService.getJoke());
+        return "index";
+    }
+}
---
+++ src/main/java/com/bluesoft/jokesapp/services/JokeService.java
@@ -0,0 +1,5 @@
+package com.bluesoft.jokesapp.services;
+
+public interface JokeService {
+    String getJoke();
+}
